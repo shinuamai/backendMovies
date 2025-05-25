ab75900f048b02153f83b0f061ec1d09f6f50d5d
Andrés Gómez <andresgomez@MacBook-Air-de-Andres.local>
2025-05-25 12:25:21 -0500

    Add: Microservicio de Recomendaciones

---
+++ ms-recomendaciones-service/src/main/java/com/peliculas/recomendaciones/RecomendacionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peliculas.recomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacionesServiceApplication.class, args);
+	}
+
+}
