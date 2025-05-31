d1f0bbb5aa3947993bbf5ce9dad4a547d1705a65
Andrés Gómez <andresgomez@MacBook-Air-de-Andres.local>
2025-05-30 22:30:48 -0500

    se agregan inserts para data inicial con fecha

--- ms-movie-recommendation/src/main/resources/data.sql
+++ ms-movie-recommendation/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- Insertar recomendaciones
-INSERT INTO recomendaciones (usuario_id, pelicula_id, razon_recomendacion, puntuacion_predicha, visualizada) VALUES
-(1, 1, 'Te gusta la ciencia ficción y Christopher Nolan', 4.5, false),
-(1, 2, 'Película clásica de ciencia ficción similar a tus preferencias', 4.2, false),
-(2, 3, 'Tu género favorito: crimen y thriller', 4.8, true),
-(2, 4, 'Obra maestra del cine que todo cinéfilo debe ver', 4.9, false),
-(3, 5, 'Combina ciencia ficción con drama familiar', 4.6, false),
-(3, 1, 'Director reconocido por narrativas complejas', 4.4, true);
+INSERT INTO recomendaciones (usuario_id, pelicula_id, razon_recomendacion, puntuacion_predicha, visualizada, fecha_recomendacion) VALUES
+(1, 101, 'Te gusta la ciencia ficción y Christopher Nolan', 4.5, false, NOW()),
+(1, 102, 'Película clásica de ciencia ficción similar a tus preferencias', 4.2, false, NOW()),
+(2, 103, 'Tu género favorito: crimen y thriller', 4.8, true, NOW()),
+(2, 104, 'Obra maestra del cine que todo cinéfilo debe ver', 4.9, false, NOW()),
+(3, 105, 'Combina ciencia ficción con drama familiar', 4.6, false, NOW()),
+(3, 106, 'Director reconocido por narrativas complejas', 4.4, true, NOW());
