38116081c864c4b89ebeb969c38f44a49146bc73
Oscar Sanabria <daniloos2000@gmail.com>
2025-06-17 19:09:23 -0500

    feat: update CORS configuration and enhance movie data in import.sql

---
+++ gateway-server/src/main/java/com/unir/gateway_server/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.unir.gateway_server.config;
+
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:5173"); // O especifica tu dominio frontend
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
